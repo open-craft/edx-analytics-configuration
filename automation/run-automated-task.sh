ffc5fe8d610f7b144a7c3237da6056dc8ea517d2
Omar Khan <omar@opencraft.com>
2016-04-04 12:27:36 +0700

    Just set ANSIBLE_EC2_INI in jenkins config

--- automation/run-automated-task.sh
+++ automation/run-automated-task.sh
@@ -27,9 +27,6 @@ fi
 . $TASKS_BIN/activate
 make -C analytics-tasks install
 
-# Use local ec2 config, if available
-[ -e ec2.ini ] && export ANSIBLE_EC2_INI="$(readlink --canonicalize --no-newline ec2.ini)"
-
 # Define task on the command line, including the task name and all of its arguments.
 # All arguments provided on the command line are passed through to the remote-task call.
 remote-task --job-flow-name="$CLUSTER_NAME" --repo $TASKS_REPO --branch $TASKS_BRANCH --wait --log-path $WORKSPACE/logs/ --remote-name automation --user $TASK_USER "$@"
